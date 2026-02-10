0725bf5b2224052de029241290416638d6aa775f
CarlosMorales <carlosmoralesrivera24@gmail.com>
2026-02-10 13:29:41 -0600

    feat(login):add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,30 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart' show RiveAnimation;
-
-
+import 'package:rive/rive.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _login_screenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _login_screenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen> {
+
+//Control para mostrar u ocultar la contraseña
+ bool _obscureText = true;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      //Evita que se quite espacio del nudge
+    //para obtener el tamaño de la pantalla y usarlo para ajustar el diseño
+    final Size size = MediaQuery.of(context).size;
+ 
+    return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded ( 
-              child: RiveAnimation.asset('assets/animated_login_bear.riv'),
-
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset('assets/animated_login_bear.riv')),
+              //Para separacion
+              const SizedBox(height: 10),
+              TextField(
+                //Para mostrar un tipo de tecleado
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    //Para redondear los bordes del campo de texto
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: Icon(Icons.lock),//cerrado o seguro
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () { //icono para mostrar u ocultar la contraseña
+                      setState(() {//Para actualizar la interfaz de usuario
+                        //Cambiar el estado de la visibilidad de la contraseña
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-    ) ;
+    );
   }
 }
